443f845175539cc74927fac56c82501424dacc98
Rashin Harisi <rashin.harisi@gmail.com>
2025-07-04 16:45:40 +0200

    developing navbar,showcase,and tech sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Hearo3DModel from "./components/3dModel/Hearo3DModel.jsx";
 import Hero from "./sections/Hero.jsx";
 import Showcase from "./sections/Showcase.jsx";
 import Navbar from "./sections/Navbar.jsx";
